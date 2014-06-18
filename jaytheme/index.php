c1951170f7ccfffe5b2de108d9883cdb6222e1d9
Jason <jasonrmajors@gmail.com>
2014-06-18 10:19:53 -0700

    Made the blog page prettier and commented page-tournament.

--- jaytheme/index.php
+++ jaytheme/index.php
@@ -1,18 +1,17 @@
+<div id='duck'>
 <?php get_header() ?>
-        <div class='content'>
-            <h1 class='title'><?php bloginfo('name'); ?> | <?php is_home() ? bloginfo('description') : wp_title(''); ?></h1>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <div id="the-post">
-                    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                    <p><?php the_content(); ?></p>
-                    <p>Posted: <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></p>
-                </div>
-                <div id="the-comment">
-                    <?php comments_number() ?>
-                </div>  
-            <?php endwhile; else: ?>
-                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-            <?php endif; ?>
-        </div>      
-        </body>
-    </html> 
+    <div class='content'>
+        <h1 class='title'><?php bloginfo('name'); ?> | <?php is_home() ? bloginfo('description') : wp_title(''); ?></h1>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <div class="post">
+                <h2 id="post-center"><?php the_title(); ?></h2>
+                <p id="post-center"><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></p>
+                <p><?php the_content(); ?></p>
+            </div>  
+        <?php endwhile; else: ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+    </div> 
+</div>         
+</body>
+</html> 
--- jaytheme/page-tournament.php
+++ jaytheme/page-tournament.php
@@ -1,10 +1,14 @@
+<!-- Check if a user is logged in -->
 <?php if (is_user_logged_in()) : ?>
+<!-- Loads header.php -->
 <?php get_header(); ?>
 <div class="content">
 	<h1>THE TOURNAMENT</h1>
 
 <?php
-	class Team
+	// Simple class to hold a Hero's name and a single attribute.
+	// Could expand to hold multiple attributes.
+	class Hero
 	{
 		public function __construct($attr, $name)
 		{
@@ -13,30 +17,33 @@
 		}
 	}
 
+	// Builds a matchup of two heroes.
 	class Match
 	{
-		public function __construct($teamA, $teamB)
+		public function __construct($heroA, $heroB)
 		{
-			$this->teamA = $teamA;
-			$this->teamB = $teamB;
+			$this->heroA = $heroA;
+			$this->heroB = $heroB;
 			$this->winner = none;
 		}
-
+		// Set a match winner based off attribute(s)
 		public function battle()
 		{
-			if ($this->teamA->attr == $this->teamB->attr) {
-				$this->winner = $this->teamA;
+			// heroA wins ties for now.
+			if ($this->heroA->attr == $this->heroB->attr) {
+				$this->winner = $this->heroA;
 			}
-			elseif ($this->teamA->attr > $this->teamB->attr) {
-				$this->winner = $this->teamA;
+			elseif ($this->heroA->attr > $this->heroB->attr) {
+				$this->winner = $this->heroA;
 			} else {
-				$this->winner = $this->teamB;
+				$this->winner = $this->heroB;
 			}
 
 			return $this->winner;
 		}
 	}
 
+	// Object to hold a set of matches.
 	class Round
 	{
 		public function __construct(array $matches)
@@ -45,12 +52,13 @@
 			$this->winners = array();
 		}
 
+		// Iterate through matches, see who wins via Hero->battle(), return an array of the winners.
 		public function play_round()
 		{
 			foreach ($this->matches as $match) {
 				$winner = $match->battle();
 				array_push($this->winners, $winner);
-				$msg = $match->teamA->name . ' vs. ' . $match->teamB->name;
+				$msg = $match->heroA->name . ' vs. ' . $match->heroB->name;
 				$winner_msg = "<em>$winner->name wins!</em>";
 				echo $msg;
 				echo "<br>";
@@ -61,38 +69,43 @@
 		}
 	}
 
+
 	class Tournament
 	{
 		public function __construct()
 		{
-			$this->teams_remaining = array();
+			$this->heroes_remaining = array();
 		}
 
+		// Place the heroes remaining (heroes_remaining array) in the tournament into matches and those matches into a round.
+		// Get the winners of the round and set that to the heroes remaining.
 		public function solve_round()
 		{
 			$matches = array();
-			for ($i = 0; $i < count($this->teams_remaining); $i = $i + 2) {
-				$teamA = $this->teams_remaining[$i];
-				$teamB = $this->teams_remaining[$i + 1];
-				$match = new Match($teamA, $teamB);
+			for ($i = 0; $i < count($this->heroes_remaining); $i = $i + 2) {
+				$heroA = $this->heroes_remaining[$i];
+				$heroB = $this->heroes_remaining[$i + 1];
+				$match = new Match($heroA, $heroB);
 				array_push($matches, $match);
 			}
 			$round = new Round($matches);
 			$winners = $round->play_round();
-			$this->teams_remaining = $winners;
+			$this->heroes_remaining = $winners;
 
 			return $winners;
 		}
 
+		// Keep solving rounds until there is a winner.
 		public function tournament()
 		{
-			if (count($this->teams_remaining) % 4 != 0) {
+			// Bracket needs to be 4**x.
+			if (count($this->heroes_remaining) % 4 != 0) {
 				exit("Invalid bracket -- must be a power of 4!");
 			}
 
 			$round_num = 1;
 
-			while (count($this->teams_remaining) > 1) {
+			while (count($this->heroes_remaining) > 1) {
 				echo "<strong>Round Number $round_num</strong></br></br>";
 				$round_winners = $this->solve_round();
 				
@@ -103,25 +116,35 @@
 				}
 
 				$round_num += 1;
-				}
-
+			}
 		}
 	}
 
-	$tournament = new Tournament();
-	$heroes = array( 'post_type' => 'my_heroes' );
-	$loop = new WP_Query( $heroes );
-	while ( $loop->have_posts() ) {
-		$loop->the_post();
-		$power = get_field( 'stats' );
-		$name = the_title('', '', false);
-		$hero = new Team($power, $name);
-		array_push($tournament->teams_remaining, $hero);
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		$tournament = new Tournament();
+		$heroes = array( 'post_type' => 'my_heroes' );
+		$loop = new WP_Query( $heroes );
+		// Create the heroes from the Hero custom post type and automatic custom fields.
+		// Add them to the heroes_remaining array.
+		while ( $loop->have_posts() ) {
+			$loop->the_post();
+			$power = get_field( 'stats' );
+			$name = the_title('', '', false);
+			$hero = new Hero($power, $name);
+			array_push($tournament->heroes_remaining, $hero);
+		}
+		$tournament->tournament();
 	}
-	$tournament->tournament();
-
-
 ?>
 
+
 <?php endif; ?>
+	<div>
+		<form method="POST" action="http://localhost/wp/tournament/">
+			<input type="hidden" name="game_on" value="">
+			<?php if (!isset($_POST["game_on"])) : ?>
+				<input type="submit" value="Lets Go!!">
+			<?php endif; ?>	
+		</form>
+	</div>		
 </div>
